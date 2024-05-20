e7a008e2d83d256bae2ae6107b2d096964ce51de
Oyyanv <muhammadroyyan527@gmail.com>
2024-05-20 23:07:52 +0800

    wip login1

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:knowledge/screens/intro/introduction.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,12 +19,81 @@ class _LoginPageState extends State<LoginPage> {
           Color(0xffBCCFFF),
           Color(0xff8EB3FA)
         ], begin: Alignment.topLeft, end: Alignment.bottomCenter)),
-        child: const SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+        child: SizedBox.expand(
+          child: Row(
             children: [
-              
-            ],),
+              //button kembali
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const Introduction();
+                      }));
+                    },
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Back',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              //button kembali
+              //knowledge icon
+              Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const SizedBox(
+                        height: 125,
+                      ),
+                      Image.asset(
+                        'lib/assets/images/logonnew.png',
+                        height: 60,
+                        width: 60,
+                      ),
+                    ],
+                  ),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      SizedBox(
+                        height: 135,
+                      ),
+                      Text(
+                        '  Knowledge',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff404080),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              //knowledge icon
+              //card login
+            ],
+          ),
         ),
       ),
     );
