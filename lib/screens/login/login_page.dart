fdd449745e34e96c37cf196578581b4132d98422
Oyyanv <muhammadroyyan527@gmail.com>
2024-05-21 12:23:08 +0800

    design page login selesai

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:knowledge/screens/intro/introduction.dart';
 
@@ -9,6 +11,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _loginKey = GlobalKey<FormState>();
+  final _username = TextEditingController();
+  final _password = TextEditingController();
+  bool _rememberMe = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +23,19 @@ class _LoginPageState extends State<LoginPage> {
         children: [
           Container(
             decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color(0xff7D99E7),
-                Color(0xffBCCFFF),
-                Color(0xff8EB3FA)
-              ], begin: Alignment.topLeft, end: Alignment.bottomCenter),
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff7D99E7),
+                  Color(0xffBCCFFF),
+                  Color(0xff8EB3FA)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomCenter,
+              ),
             ),
           ),
           Row(
             children: [
-              //button kembali
               Column(
                 children: [
                   const SizedBox(
@@ -58,7 +68,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ],
           ),
-          //knowledge icon
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -95,7 +104,187 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ],
           ),
-          //knowledge icon
+          // Box login
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              Container(
+                padding: const EdgeInsets.all(4),
+                width: 500,
+                height: 455,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 166, 166, 166),
+                      spreadRadius: 0,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0XFF404080),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Form(
+                      key: _loginKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 40),
+                            child: Text(
+                              'Username',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0XFFC7C5C1),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(3),
+                            margin: EdgeInsets.symmetric(horizontal: 20),
+                            width: double.infinity,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: Color(0xffECECEC),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50),
+                              ),
+                            ),
+                            child: TextFormField(
+                              controller: _username,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding:
+                                    EdgeInsets.symmetric(horizontal: 20),
+                                hintText: 'Username',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 40),
+                            child: Text(
+                              'Password',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0XFFC7C5C1),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(3),
+                            margin: EdgeInsets.symmetric(horizontal: 20),
+                            width: double.infinity,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: Color(0xffECECEC),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50),
+                              ),
+                            ),
+                            child: TextFormField(
+                              controller: _password,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding:
+                                    EdgeInsets.symmetric(horizontal: 20),
+                                hintText: 'Password',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Row(
+                              children: [
+                                Checkbox(
+                                  value: _rememberMe,
+                                  activeColor: Colors.grey,
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      _rememberMe = value ?? false;
+                                    });
+                                  },
+                                ),
+                                Text('Remember me')
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Center(
+                            child: Container(
+                              margin: EdgeInsets.symmetric(horizontal: 20),
+                              width: double.infinity,
+                              decoration: const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xff6B6BA6),
+                                    Color(0xff6579C1),
+                                    Color(0xff404080)
+                                  ],
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(30),
+                                ),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Sign In',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //box login
         ],
       ),
     );
